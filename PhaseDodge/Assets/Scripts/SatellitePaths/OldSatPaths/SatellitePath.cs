685b41ca533a8fc8251973375effbe601f18eddf
LuckyLoui-C <dan@dancampbell.com.au>
2025-04-14 22:32:54 +1000

    Just the build

--- PhaseDodge/Assets/Scripts/SatellitePaths/OldSatPaths/SatellitePath.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SatellitePath", menuName = "Scriptable Objects/SatellitePath", order = 1)]
-public class SatellitePath : ScriptableObject
-{
-    public Vector3[] waypoints;
-
-}
