6a0e21e730dbfa2c2e2aee0354844f67677c7f50
LuckyLoui-C <dan@dancampbell.com.au>
2025-04-14 22:20:54 +1000

    Revert "Updated Repo for web embed"
    This reverts commit badc0fffbb0a695db08ed72d5b63e2845f6c71d7.

---
+++ PhaseDodge/Assets/Scripts/UI/GameplayUIManager.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System;
+
+public class GameplayUIManager : MonoBehaviour
+{
+    [SerializeField] private Button pauseButton;
+    [SerializeField] private Button resumeButton;
+    [SerializeField] private Button retryButton;
+    [SerializeField] private Button[] quitButtons;
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject gameOverMenu;
+
+    [SerializeField] private TextMeshProUGUI scoreText;
+
+    void Start()
+    {
+        pauseButton.onClick.AddListener(PauseGame);
+        retryButton.onClick.AddListener(RetryGame);
+        resumeButton.onClick.AddListener(ResumeGame);
+        foreach (Button quitButton in quitButtons)
+        {
+            quitButton.onClick.AddListener(ReturnToMainMenu);
+        }
+        pauseMenu.SetActive(false);
+    }
+
+    private void RetryGame()
+    {
+        SceneManager.LoadScene("BuildTestScene");
+        Time.timeScale = 1;
+    }
+
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+
+    private void ReturnToMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenuScene");
+    }
+
+    public void UpdateScore(string formattedTime)
+    {
+        scoreText.text = "Score: " + formattedTime;
+    }
+
+    public void ShowGameOverMenu()
+    {
+        gameOverMenu.SetActive(true);
+    }
+}
