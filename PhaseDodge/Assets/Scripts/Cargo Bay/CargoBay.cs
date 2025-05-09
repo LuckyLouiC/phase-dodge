c3f79ab91c8084720613e9da1d62f541a80d34db
LuckyLoui-C <dan@dancampbell.com.au>
2025-05-09 11:01:58 +1000

    Added CargoBay.cs - Ship Inventory Stores Resources
    - The Cargo Bay functions as the ship's inventory
    - Miner sends raw resources to the cargo bay
    -Refinery takes raw resources from the cargo bay, refines them, and sends refined resources to the cargo bay

---
+++ PhaseDodge/Assets/Scripts/Cargo Bay/CargoBay.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CargoBay : MonoBehaviour
+{
+    // The Cargo Bay functions as the ship's inventory
+    // Miner sends raw resouces to the cargo bay
+    // Refinery takes raw resources from the cargo bay,
+    // refines them, and sends refined resources to the cargo bay
+
+
+
+}
